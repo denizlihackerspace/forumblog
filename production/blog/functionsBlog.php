edb78cff3fd89cff9cc48f15da25df1fdfdb75ef
YunusEmre <ynsmrrkn@gmail.com>
2016-08-04 10:57:02 +0300

    commit

---
+++ production/blog/functionsBlog.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: YunusEmre
+ * Date: 4.8.2016
+ * Time: 10:40
+ */
+
+//blog fonksiyonlarının deposu
+
+
+?>
--- production/blog/index.php
+++ production/blog/index.php
@@ -17,10 +17,10 @@
     <title>Blog hakerspace</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../documentation/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="css/blog-home.css" rel="stylesheet">
+    <link href="../../documentation/css/blog-home.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -217,10 +217,10 @@
     <!-- /.container -->
 
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <script src="../../documentation/js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="../../documentation/js/bootstrap.min.js"></script>
 
 </body>
 
